a0046ed9594eb9c599cfa49016215f5349ecd3d1
Steven Zaldivar <zh15002@ues.edu.sv>
2021-01-21 00:11:23 -0600

    Adding project structure

---
+++ src/main/java/sv/edu/ues/webhook/DialogflowWebhookApplication.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialogflowWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DialogflowWebhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sv/edu/ues/webhook/DialogflowWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.ues.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialogflowWebhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
