fd10b4379e37837ad9274c9d40059aa341004b43
Steven Zaldivar <zh15002@ues.edu.sv>
2021-01-21 00:58:58 -0600

    Add entrypoint controller for fullfilment

---
+++ src/main/java/sv/edu/ues/webhook/controllers/EntrypointController.java
@@ -0,0 +1,32 @@
+package sv.edu.ues.webhook.controllers;
+
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.dialogflow.v2beta1.model.GoogleCloudDialogflowV2WebhookRequest;
+import com.google.api.services.dialogflow.v2beta1.model.GoogleCloudDialogflowV2WebhookResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class EntrypointController {
+
+    private static JacksonFactory factory = JacksonFactory.getDefaultInstance();
+
+    @PostMapping
+    public GoogleCloudDialogflowV2WebhookResponse main(@RequestBody String body) throws IOException {
+
+        GoogleCloudDialogflowV2WebhookResponse response = new GoogleCloudDialogflowV2WebhookResponse();
+
+        GoogleCloudDialogflowV2WebhookRequest request =
+                factory.createJsonParser(body).parse(GoogleCloudDialogflowV2WebhookRequest.class);
+
+        System.out.println("[iNFO-INTENT]: "+request.getQueryResult().getIntent().getDisplayName());
+        if(request.getQueryResult().getIntent().getDisplayName().equals("Eventos")){
+            response.setFulfillmentText("Respuesta con spring webhook");
+        }
+        return response;
+    }
+
+}
