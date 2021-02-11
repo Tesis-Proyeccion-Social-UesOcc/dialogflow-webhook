a65001c587c1f8ad4c28c40172a22188076ac86f
Steven Zaldivar <zh15002@ues.edu.sv>
2021-02-11 01:20:38 -0600

    Update MethodData class docstring

--- src/main/java/sv/edu/ues/webhook/annotations/MethodData.java
+++ src/main/java/sv/edu/ues/webhook/annotations/MethodData.java
@@ -5,8 +5,8 @@ import sv.edu.ues.webhook.services.Handler;
 import java.lang.reflect.Method;
 
 /**
- * Class to act as a container for the method with {@link IntentHandler} and its container object
- * */
+ * Class to act as a container for methods annotated with {@link IntentHandler} and its container object
+ */
 public class MethodData {
     private final Method method;
     private final Handler target;
