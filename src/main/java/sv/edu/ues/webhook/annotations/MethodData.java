20962f1372b1d25ad1040f2e3ccf8a446e6806c7
Steven Zaldivar <zh15002@ues.edu.sv>
2021-02-11 00:22:50 -0600

    Add MethodData class
    That class will be used as a mapper between method with
    the annotation IntentHandler and its container object

---
+++ src/main/java/sv/edu/ues/webhook/annotations/MethodData.java
@@ -0,0 +1,26 @@
+package sv.edu.ues.webhook.annotations;
+
+import sv.edu.ues.webhook.services.Handler;
+
+import java.lang.reflect.Method;
+
+/**
+ * Class to act as a container for the method with {@link IntentHandler} and its container object
+ * */
+public class MethodData {
+    private final Method method;
+    private final Handler target;
+
+    public MethodData(Method method, Handler target) {
+        this.method = method;
+        this.target = target;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Handler getTarget() {
+        return target;
+    }
+}
---
+++ src/main/java/sv/edu/ues/webhook/services/Handler.java
@@ -0,0 +1,5 @@
+package sv.edu.ues.webhook.services;
+
+public interface Handler {
+    void handle();
+}
